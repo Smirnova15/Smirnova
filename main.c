1331194498cd5ace87756231db143ace3e35c9c2
Evgenia Smirnova <jeniasmirn@rambler.ru>
2017-09-19 14:04:49 +0300

    Revert "Give a personalized hello"
    This reverts commit d856a294f1d829dc53836d133dcc5ea5501c09ec.
    
    # Conflicts:
    #	main.c

--- main.c
+++ main.c
@@ -8,7 +8,11 @@ int main(int argc, char **argv)
 
     askname(first, last);
 
+<<<<<<< HEAD
     printf("Hello, %s %s!\n", first, last);
+=======
+    printf("Hello %s!\n", name);
+>>>>>>> parent of d856a29... Give a personalized hello
     return 0;
 }
 
